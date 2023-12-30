7164da362f9052f6ce4d6b3176dc30fd166d554b
Apekshakv <153891838+Apekshakv@users.noreply.github.com>
2023-12-30 19:27:09 +0530

    Add files via upload

---
+++ apeksha.js
@@ -0,0 +1,79 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+
+let turnO = true; // playerX, playerO
+let count = 0; // To Track Draw
+
+const winPatterns = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [0, 4, 8],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+
+boxes.forEach((box) => {
+  box.addEventListener("click", () => {
+    if (box.innerText === "") { // Check if the box is empty
+      if (turnO) {
+        box.innerText = "O";
+        turnO = false;
+      } else {
+        box.innerText = "X";
+        turnO = true;
+      }
+      count++;
+
+      if (checkWinner()) {
+        displayMessage(`Player ${turnO ? "X" : "O"} wins!`);
+        disableBoxes();
+      } else if (count === 9) {
+        displayMessage("It's a draw!");
+      }
+    }
+  });
+});
+
+function checkWinner() {
+  for (const pattern of winPatterns) {
+    const [a, b, c] = pattern;
+    if (
+      boxes[a].innerText &&
+      boxes[a].innerText === boxes[b].innerText &&
+      boxes[a].innerText === boxes[c].innerText
+    ) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function displayMessage(message) {
+  msg.textContent = message;
+  
+}
+
+
+
+
+    
+
+
+
+  
+
+
+
+
+  
+
+
+
+
+
